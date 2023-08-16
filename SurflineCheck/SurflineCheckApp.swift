41308eaeaf4b9239c9dd294ed58563dee00189e3
Murphy McQuet <murphymcquet@gmail.com>
2023-08-16 10:01:44 -0600

    Initial Commit

---
+++ SurflineCheck/SurflineCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurflineCheckApp.swift
+//  SurflineCheck
+//
+//  Created by Murphy McQuet on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SurflineCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
